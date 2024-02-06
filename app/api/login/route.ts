6d63a91fe7cc63eb381ca3108c39562efc16d70f
guirab <guilhermesr2000@gmail.com>
2024-02-06 17:27:31 -0300

    test: test frontEnd components

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -10,15 +10,7 @@ export async function POST(req: NextRequest) {
       driver: sqlite3.Database
     });
   }
-  let data
-  try {
-    data = await req.json();
-  } catch (e) {
-    data= req.body
-    return NextResponse.json({error: "Invalid JSON"});
-  }
-  // const data = await req.json();
-  console.log(data)
+  const data = await req.json();
   const result = await db.all('SELECT * FROM accounts WHERE username = ?', data.username);
   if(result.length === 0){
     return NextResponse.json({error: "Invalid username"});
